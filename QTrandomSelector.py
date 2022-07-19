9832efe181ada00ae69dc6a646e6fc8f12ceb021
jogeuncheol <84510674+jogeuncheol@users.noreply.github.com>
2022-07-20 01:50:38 +0900

    [add] .ui packaging

--- QTrandomSelector.py
+++ QTrandomSelector.py
@@ -4,9 +4,18 @@ import random
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
+
 # UI파일 연결
 # 단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
-form_class = uic.loadUiType("randomOpen.ui")[0]
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    # 출처: https://editor752.tistory.com/140 [CF::LF:티스토리]
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+
+form = resource_path('randomOpen.ui')
+form_class = uic.loadUiType(form)[0]
 
 
 # 화면을 띄우는데 사용되는 Class 선언
